b696df747052acc034fbd083bacc75283e46469c
Kingshuk Dash <kingshukdash123@gmail.com>
2026-01-07 00:51:23 +0530

    fix header dir in index.js

--- mega-blog/src/components/index.js
+++ mega-blog/src/components/index.js
@@ -1,17 +1,17 @@
-import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Container from "./container/Container";
-import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
-import Signup from "./Signup";
-import Login from "./Login";
-import Button from "./Button";
-import PostForm from "./PostForm/PostForm";
-import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+import Select from "./Select.jsx";
+import Header from './header/Header.jsx'
+import Footer from "./Footer/footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import Button from "./Button.jsx";
+import PostForm from "./PostForm/PostForm.jsx";
+import PostCard from "./PostCard.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import Input from "./Input.jsx";
 export {
     Header,
     Footer,
