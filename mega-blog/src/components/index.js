c5a03ccde04beee0a3842670016043686d97e0d9
Kingshuk Dash <kingshukdash123@gmail.com>
2026-01-07 00:54:21 +0530

    fix footer dir in index.js 2

--- mega-blog/src/components/index.js
+++ mega-blog/src/components/index.js
@@ -1,6 +1,6 @@
 import Select from "./Select.jsx";
 import Header from './header/Header.jsx'
-import Footer from "./footer/footer.jsx";
+import Footer from "./footer/Footer.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./header/LogoutBtn.jsx";
